4d10ba23df15e59af127b855e3dde298144cd26b
hainag16 <36581076+hainag16@users.noreply.github.com>
2020-09-25 22:46:41 +0530

    UI Updates

--- src/components/BusSearch.js
+++ src/components/BusSearch.js
@@ -6,6 +6,23 @@ export default function BusSearch() {
  
     const [displayBusLayout , setDisplayBusLayout] = useState(false);
     const [displayTravelInfo , setDisplayTravelInfo] = useState(false);
+    const [displayBoardingPointInfo , setDisplayBoardingPointInfo] = useState(true);
+    const [displayDroppingPointInfo , setDisplayDroppingPointInfo] = useState(false);
+    const [source , setSource] = useState("");
+    const [destination , setDestination] = useState("");
+    const [selectedSeats , setSelectedSeats] = useState([]);
+
+    function handleStateChanges(event) {
+
+        let target = event.target;
+        if(target.name === 'source'){
+            setSource(target.value);
+        }
+        else if(target.name === "destination"){
+            setDestination(target.value);
+        }
+
+    }
 
     return (
         <div className="BusSearch">
@@ -71,40 +88,122 @@ export default function BusSearch() {
                                                 <span className="BusDriverFontCss">D</span>
                                                 <div className="seatsRow1Css">
                                                     <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S1</span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S2</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S3</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S4</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S5</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S6</span>
                                                 </div>
                                                 <div className="seatsRow2Css">
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S7</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S8</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S9</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S10</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S11</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S12</span>
                                                 </div>
                                                 <div className="seatsRow3Css">
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S13</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S14</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S15</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S16</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S17</span>
+                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss">S18</span>
                                                 </div>
                                                 <div className="seatsRow4Css">
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
-                                                    <span onClick={()=>setDisplayTravelInfo(true)} className="BusSeatsCss"></span>
+                                                    <span onClick={()=>{setDisplayTravelInfo(true);setSelectedSeats([...selectedSeats,"S19"])}} className="BusSeatsCss">S19</span>
+                                                    <span onClick={()=>{setDisplayTravelInfo(true);setSelectedSeats([...selectedSeats,"S20"])}} className="BusSeatsCss">S20</span>
+                                                    <span onClick={()=>{setDisplayTravelInfo(true);setSelectedSeats([...selectedSeats,"S21"])}} className="BusSeatsCss">S21</span>
+                                                    <span onClick={()=>{setDisplayTravelInfo(true);setSelectedSeats([...selectedSeats,"S22"])}} className="BusSeatsCss">S22</span>
+                                                    <span onClick={()=>{setDisplayTravelInfo(true);setSelectedSeats([...selectedSeats,"S23"])}} className="BusSeatsCss">S23</span>
+                                                    <span onClick={()=>{setDisplayTravelInfo(true);setSelectedSeats([...selectedSeats,"S24"])}} className="BusSeatsCss">S24</span>
                                                 </div>
                                             </div>
                                     </div>
                                     {
-                                        (displayTravelInfo=== true)?<div className="selectedSeatsInfoCss">working</div>:""
+                                        (displayTravelInfo=== true)?
+
+                                        <div className="selectedSeatsInfoCss">
+                                            <div className="selectedSeatsInfoCss1">
+                                                {
+                                                    (displayBoardingPointInfo=== true)?
+                                                    <div>
+                                                        <h2 style={{textAlign : "center",color:"#d84f57"}}>BOARDING POINT</h2>
+                                                        <hr />
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>{setDisplayBoardingPointInfo(false);setDisplayDroppingPointInfo(true)}} value="BoardingPoint1" type="radio" name="source" />
+                                                            <label>BoardingPoint 1</label>
+                                                        </div>
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>{setDisplayBoardingPointInfo(false);setDisplayDroppingPointInfo(true)}} value="BoardingPoint2" type="radio" name="source" />
+                                                            <label>BoardingPoint 2</label>
+                                                        </div>
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>{setDisplayBoardingPointInfo(false);setDisplayDroppingPointInfo(true)}} value="BoardingPoint3" type="radio" name="source" />
+                                                            <label>BoardingPoint 3</label>
+                                                        </div>
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>{setDisplayBoardingPointInfo(false);setDisplayDroppingPointInfo(true)}} value="BoardingPoint4" type="radio" name="source" />
+                                                            <label>BoardingPoint 4</label>
+                                                        </div>
+                                                    </div>
+                                                    :
+                                                    
+                                                    (displayDroppingPointInfo === true )?
+                                                    <div>
+                                                        <h2 style={{textAlign : "center",color:"#d84f57"}}>DROPPING POINT</h2>
+                                                        <hr />
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>setDisplayDroppingPointInfo(false)} value="DroppingPoint1" type="radio" name="destination" />
+                                                            <label>DroppingPoint 1</label>
+                                                        </div>
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>setDisplayDroppingPointInfo(false)} value="DroppingPoint2" type="radio" name="destination" />
+                                                            <label>DroppingPoint 2</label>
+                                                        </div>
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>setDisplayDroppingPointInfo(false)} value="DroppingPoint3" type="radio" name="destination" />
+                                                            <label>DroppingPoint 3</label>
+                                                        </div>
+                                                        <div>
+                                                            <input onChange={handleStateChanges} onClick={()=>setDisplayDroppingPointInfo(false)} value="DroppingPoint4" type="radio" name="destination" />
+                                                            <label>DroppingPoint 4</label>
+                                                        </div>
+                                                    </div>
+                                                    : 
+                                                    <div>
+                                                        <h2 style={{textAlign : "center",color:"#d84f57"}}>TRAVEL INFO</h2>
+                                                        <hr />
+                                                        <div style={{padding:"20px"}}>
+                                                            <div className = "travelInfoCss">
+                                                                <div>Boarding Point</div>
+                                                                <div>{source}</div>
+                                                            </div>
+                                                            <div className = "travelInfoCss">
+                                                                <div>Dropping Point</div>
+                                                                <div>{destination}</div>
+                                                            </div>
+                                                            <div className = "travelInfoCss">
+                                                                <div>Selected Seats</div>
+                                                                <div>{selectedSeats.map(value=><span>{value}</span>)}</div>
+                                                            </div>
+                                                            <div className = "travelInfoCss">
+                                                                <div>Fare</div>
+                                                                <div>INR 1500.00</div>
+                                                            </div>
+                                                            <div>
+                                                                <button style={{width:"100%",backgroundColor : "#d84f57",color:"white"}}>PROCEED TO BOOK</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+
+                                                }
+                                            </div>
+                                        </div>
+
+                                        :
+                                        ""
                                     }
                                 </div>
                                 : 
